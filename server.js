5ef752d59eefdb09db3624b93acba53ca9abb5d7
sandro <92390693+sandro-olgesashvili@users.noreply.github.com>
2022-11-27 19:49:55 +0400

    update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
 require("dotenv").config();
 require("express-async-errors");
 const { connectDB } = require("./config/db");
@@ -8,13 +10,15 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use("/api/v1/goals", require("./routes/goalRoutes"));
 app.use("/api/v1/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
-
 
 connectDB().then(() => {
   app.listen(port, () => {
